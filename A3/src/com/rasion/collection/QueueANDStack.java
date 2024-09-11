4ba5f53abdceac98b46bb268db75567c9742f346
Rasioner <2806260754@qq.com>
2024-09-11 20:36:48 +0800

    List集合：ArrayList集合，基于数组，内存占用少；LinkList集合，基于链表，建队列和栈

---
+++ A3/src/com/rasion/collection/QueueANDStack.java
@@ -0,0 +1,28 @@
+package com.rasion.collection;
+
+import java.util.LinkedList;
+
+public class QueueANDStack {
+    public static void main(String[] args) {
+        LinkedList<String> queue = new LinkedList<>();
+        //入队
+        queue.addFirst("1");
+        queue.addLast("2");
+        queue.addFirst("3");
+        queue.addLast("4");
+        System.out.println(queue);
+        //出队
+        System.out.println(queue.removeFirst());
+        System.out.println(queue.removeLast());
+        System.out.println(queue);
+
+        LinkedList<String> stack = new LinkedList<>();
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+        System.out.println(stack);
+        System.out.println(stack.pop());
+        System.out.println(stack);
+    }
+}
